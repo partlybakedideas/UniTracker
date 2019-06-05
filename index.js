ffae251925c9ba6e60140f2392b718c180fd2225
partlybakedideas <46286851+partlybakedideas@users.noreply.github.com>
2019-06-04 19:33:00 -0600

    adding pug.

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 var express = require('express');
 var app = express();
+var pug = require('pug');
 var bodyParser = require('body-parser');
 app.use(express.static('public'));
-
+app.set("view engine", "pug");
+var result;
+//ReadPosts();
+//var currentsposts = ReadPosts();
+app.get('/', function(req, res){res.render('index')})
 var listener = app.listen(process.env.PORT, function() {
   console.log(listener.address().port);
 });
